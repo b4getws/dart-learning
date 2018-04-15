8b2df75b63f19010a2174659e1413e5ca5cf8436
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2018-04-15 19:25:13 +0200

    update

---
+++ 01 - Basics.dart
@@ -0,0 +1,80 @@
+// Zakaria Chahboun
+/*
+12/04/2018
+13/04/2018
+*/
+
+import 'dart:async';
+import 'dart:io';
+
+class Car {
+	sayHello(String name)=> print("Hello "+name);
+}
+
+void main() {
+
+// Read File
+/*
+// to use (async/await) you must return a 'Future' type function body
+Future foo() async{
+	try {
+
+			var des = await new File('C:/Users/hp/Desktop/text.txt').readAsString();
+			// to do not use (async/await) : use promise ".then(val){...}"
+			print(des);
+
+	} on IOException catch (e) {
+	print('Could not describe object: $e');
+	}
+}
+
+foo();
+*/
+
+// Define Class
+Car mcar = new Car();
+mcar.sayHello("zakaria");
+
+// List
+var list = new List(); // var list = [1,2]
+list.add(1);
+list.add(2);
+print(list);
+
+// Map
+var map = new Map(); // var map = {1: zakaria, 2:ali}
+map[1] = "zakaria";
+map[2] = "ali";
+print(map);
+
+// Parse String to Number
+try {
+var x = num.parse("55");
+print(x);
+	
+} catch (e) {
+	print(e.toString());
+}
+
+// Hash Code 'len of hash = 9 :)'
+var hash = "zakaria 1996";
+print(hash.hashCode);
+
+// Number Methods
+var a = 195.9;
+print(a.ceil()); // 196
+print(a.floor()); // 195
+print(a.round()); // 196
+print(a.compareTo(14785)); // -1 'if less', 1 'if great', 0 'if eaqual'
+
+// String Methods
+var b = "zakaria,chahboun,ali,balawi";
+print(b.split(','));
+ 
+// Enumurations (show end of main())
+print(Status.values);
+
+}
+
+// Enumurations
+enum Status {none,solid,liquid,gaz}
---
+++ 02 - Classes.dart
@@ -0,0 +1,46 @@
+// Zakaria chahboun
+/* 13/04/2018 */
+
+void main(List<String> args) {
+
+  // Classe / Interface
+  Calculator calc = new Calculator();
+  var dis = calc.getDistance(10.25,15);
+  print(dis);
+  calc.printKilo();
+
+  // Functions
+  print(decorateName("zakaria"));
+  printMyInfo(10); // 10 : zaki
+  printMyInfo(10,"chahboun"); // 10 : chahboun
+
+}
+
+// functions
+String decorateName(String name){
+  return "*** "+name+" ***";
+}
+
+void printMyInfo(int id, [String name = "zaki"]){ // default variabls mus be in [...]
+    print("ID: $id, Name: $name");
+}
+
+// interfaces ( = simple class)
+abstract class Distance {
+  int getDistance(double x, double y);
+}
+
+abstract class Kilo {
+  printKilo();
+}
+
+class Calculator implements Distance,Kilo{
+    int getDistance(double x, double y){
+      return (y-x).abs();
+    }
+    printKilo(){
+        //print("Kilo!");
+    }
+}
+
+//
